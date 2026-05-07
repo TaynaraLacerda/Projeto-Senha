9be1779874d26c84ead54e2b691ebdca011a9987
TaynaraLacerda <aluno@senaigama.academico.local>
2026-05-07 20:57:07 -0300

    Trabalho Concluido

--- index.py
+++ index.py
@@ -14,22 +14,12 @@ senha_atual.set("---")
 
 #Tela 2 
 
-admin = Toplevel(cliente)
+admin=Toplevel(cliente)
 admin.title("Administrador")
 admin.geometry("300x350")
 admin.configure(bg="lightgreen")
 
-# Lista da fila no admin
-
-Label(admin, text="Fila de Senhas", font=("Arial",14)).pack(pady=10)
-
-lista_admin = Listbox(admin, width=20, height=10)
-lista_admin.pack(pady=10)
-
-# Lista da fila no admin
-
-Label(admin, text="Fil de Senhas", font=("Arial", 14)).pack(pady=10)
-
+Label(admin, text="Fila de Senhas", font=("Arial", 14)).pack(pady=10)
 lista_admin = Listbox(admin, width=20, height=10)
 lista_admin.pack(pady=10)
 
@@ -41,13 +31,50 @@ painel.geometry("400x300")
 painel.configure(bg="lightgreen")
 
 Label(painel, text="Senha Atual", font=("Arial", 20)).pack(pady=20)
-
 Label (
     painel,
     textvariable=senha_atual,
-    font=("Arial",40),
+    font=("Arial", 40),
     fg="red"
-
 ).pack(pady=20)
+
+def solicitar_senha():
+    global contador
+
+    senha = f"A{contador:03}"
+    fila.append(senha)
+    lista_admin.insert(END, senha)
+    messagebox.showinfo("Senha gerada", f"Sua senha é {senha}")
+
+    contador += 1
+
+def chamar_senha():
+    if len(fila) == 0:
+        messagebox.showwarning("Aviso", "Fila vazia!")
+    else:
+        senha = fila.pop(0)
+
+        lista_admin.delete(0)
+
+        senha_atual.set(senha)
+
+    
+Label(cliente, text="Retirar senha", font=("Arial", 16)).pack(pady=20)
+
+Button(
+    cliente,
+    text="Gerar Senha",
+    width=20,
+    command=solicitar_senha
+).pack(pady=10)
+
+Button(
+    admin,
+    text="Chamar Próxima",
+    width=20,
+    command=chamar_senha
+).pack(pady=10)
+
+    # Execução 
 
 cliente.mainloop()
